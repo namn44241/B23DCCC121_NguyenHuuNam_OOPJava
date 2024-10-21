73d495a05cb5cbca493d24ea91c749ec9405d902
namn44241 <nguyenvandoaaaaa@gmail.com>
2024-10-21 14:41:08 +0700

    ADD - Code ptit 21.10

---
+++ Code_Ptit_Java/src/B27/J02005/Main.java
@@ -0,0 +1,36 @@
+package B27.J02005;
+
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main{
+    
+    static boolean has(int[] a, int n, int x){
+        for(int i=0;i<n;i++) if(a[i]==x) return true;
+        return false;
+    }
+    
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        TreeSet<Integer> h = new TreeSet<>();
+        int n = sc.nextInt(), m = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[m];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<m;i++){
+            b[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(has(b,m,a[i])){
+                h.add(a[i]);
+            }
+        }
+        h.forEach((k)->{
+            System.out.printf("%d ", k);
+        });
+        sc.close();
+    }
+}
---
+++ Code_Ptit_Java/src/B36/J02017/Main.java
@@ -0,0 +1,22 @@
+package B36.J02017;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main{
+    
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> a = new Stack<>();
+        for(int i=0; i<n; i++){
+            int x = sc.nextInt();
+            if(a.empty()) a.push(x);
+            else{
+                if((a.lastElement()+x)%2==0) a.pop();
+                else a.push(x);
+            }
+        }
+        System.out.print(a.size());
+        sc.close();
+    }
+}
